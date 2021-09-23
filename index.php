fe8321e8f17b3ae3ed89a46c6809a5ffba3fa1e6
Roy Verrips <roy@verrips.org>
2021-09-23 09:12:34 -0500

    new index.php

--- index.php
+++ index.php
@@ -1,21 +1,12 @@
 <?php
 
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
+use Psr\Http\Message\ServerRequestInterface;
 
-$uri = urldecode(
-    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
-);
+function laravel(ServerRequestInterface $request): string
+{
+    $app = require __DIR__ . '/bootstrap/app.php';
 
-// This file allows us to emulate Apache's "mod_rewrite" functionality from the
-// built-in PHP web server. This provides a convenient way to test a Laravel
-// application without having installed a "real" web server software here.
-if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
-    return false;
+    $app->run();
+
+    return '';
 }
-
-require_once __DIR__.'/public/index.php';
