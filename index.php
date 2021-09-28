9ddca72d11b354271b61d6761a72fa04b042d3ec
Roy Verrips <roy@verrips.org>
2021-09-28 11:11:40 -0500

    trying new index.php config

--- index.php
+++ index.php
@@ -4,9 +4,7 @@ use Psr\Http\Message\ServerRequestInterface;
 
 function laravel(ServerRequestInterface $request): string
 {
-    $app = require __DIR__ . '/bootstrap/app.php';
-
-    $app->run();
+    require_once __DIR__.'/public/index.php';
 
     return '';
 }
